f250483fe968e8ed263936fbffcf093253d9bd7d
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2019-11-29 21:55:07 +0100

    Update affiche_articles.php

--- projet_starcroisieres/affiche_articles.php
+++ projet_starcroisieres/affiche_articles.php
@@ -9,7 +9,7 @@ $req=$db->query('SELECT id,titre,content,image FROM articles');
 	 while($donnees = $req->fetch()){
 	  
 	echo'<div class="ter">
-	            <div><strong>title:'.$donnees['titre'].'</strong></div>
+	            <div><strong>title:'.$donnees['titre'].'</div>
 				<div><span class="dert"><img src="upload/'.$donnees['image'].'" alt="'.$donnees['image'].'"  width="150" height="100"/></span> <span class="irc">'.$donnees['content'].'</span></div>
 				<div><a href="articles_page.php?page='.$donnees['id'].'">Blog de l\'article</a> </div>
 				</div>';
